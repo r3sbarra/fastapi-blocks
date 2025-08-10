ba560e549d4b9fe33016036581ade798a8e781e4
r3sbarra <richardmark.pi@gmail.com>
2025-08-10 14:27:15 -0400

    update test with changes to block manager

--- tests/test_placeholder.py
+++ tests/test_placeholder.py
@@ -10,7 +10,7 @@ def test_tmp_file(tmp_path):
     
 def test_block_manager_init(tmp_path, test_app, tmpdir):
     tmpdir.mkdir("blocks")
-    block_manager = BlockManager(blocks_folder="blocks", working_dir=str(tmp_path), late_setup=True)
+    block_manager = BlockManager(blocks_folder="blocks", working_dir=str(tmp_path), late_load=True)
     block_manager._setup()
     
     assert os.path.exists(os.path.join(tmp_path, "blockmanager"))
