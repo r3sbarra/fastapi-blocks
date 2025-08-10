855e95f5417d044838980c1707b453314ff7200d
r3sbarra <richardmark.pi@gmail.com>
2025-08-10 15:30:48 -0400

    threading

--- src/fastapi_blocks/block_manager.py
+++ src/fastapi_blocks/block_manager.py
@@ -21,14 +21,17 @@ import toml
 import subprocess
 import sys
 import inspect
+import threading
 
 class SingletonMeta(type):
     _instances = {}
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(SingletonMeta, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
+    _lock: threading.Lock = threading.Lock()
 
+    def __call__(cls, *args, **kwargs):
+        with cls._lock:
+            if cls not in cls._instances:
+                cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
 
 # The BlockManager class is responsible for managing the blocks in the FastAPI application.
 class BlockManager(metaclass=SingletonMeta):
